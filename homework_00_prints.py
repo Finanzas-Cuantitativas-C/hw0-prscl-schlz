fc2d7ae44757b15847c743dfc90218a86c06b3f9
Priscila Schulz <priscila.schulz99@gmail.com>
2021-07-12 12:12:17 -0500

    Terminé la tarea con 10

--- homework_00_prints.py
+++ homework_00_prints.py
@@ -25,7 +25,7 @@ import requests
 
 
 #Escribe aqui tu numero de cuenta. Si tienes dudas consulta el README.md
-numeroDeCuenta = ''
+numeroDeCuenta = '315205017'
 
 
 '''
@@ -60,7 +60,7 @@ def tarea0():
     # is_normal 
     
     #If you have any questions, see our video on Youtube
-    return 
+    return round(dm.median,4),round(dm.var_95,6),dm.is_normal
 
 
 #NO MODIFICAR
